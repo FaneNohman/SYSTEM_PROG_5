063abf808a4ab9a856dc73aa7687ea85c615ae71
FaneNohman <kingofthepixels1@gmail.com>
2023-03-30 19:07:05 +0600

    first commit

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        private static object _lock = new object();
+        static void Main(string[] args)
+        {
+            new Thread(MethodForNewThread).Start();
+            new Thread(MethodForNewThread).Start();
+        }
+        private static void MethodForNewThread()
+        {
+            lock (_lock)
+            {
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
